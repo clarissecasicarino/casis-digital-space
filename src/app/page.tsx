509e795ba5c95287e30fb85a9a635e0a14c65384
clarissecasicarino <cclarissecasi@gmail.com>
2024-04-24 22:28:17 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <div className="flex-col">
+        <p className="font-semibold text-5xl lg:text-6xl text-slateBlue">
+          clarisse casi
+        </p>
+        <p className="text-slateBlue pb-3 text-lg">
+          {"["} kla-ris kah-see {"]"}
+        </p>
+        <hr className="h-0.5 bg-black w-96" />
+        <div className="pb-4" />
+        <div className="rounded-full bg-white px-2 w-14">
+          <p className="text-center">noun</p>
+        </div>
+      </div>
+    </main>
+  );
+}
